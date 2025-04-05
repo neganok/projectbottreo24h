7c83bcb06ee55449a8899eb625de93cf0f57c035
Văn Thành <hackersvn1@gmail.com>
2025-04-05 08:20:41 +0700

    Update koyeb.sh

--- koyeb.sh
+++ koyeb.sh
@@ -5,34 +5,29 @@ command -v koyeb &>/dev/null || (echo "Cài đặt Koyeb CLI..."; curl -fsSL htt
 
 export PATH=$HOME/.koyeb/bin:$PATH
 export KOYEB_TOKEN=f66m2frs9zjsfdyjrelo82l7qn64fakjgfgnnafdmw6qfl1p9h55ue5nmlo7fs80
-REDEPLOY_COUNT=1
 
-# Redeploy dịch vụ
-redeploy_service() {
-  echo "Tạm dừng và resume dịch vụ wrong-alexia/setup..."
-  koyeb service pause wrong-alexia/setup && sleep 20
-  koyeb service resume wrong-alexia/setup && sleep 5
-  # Chỉ thực hiện redeploy từ lần 1 trở đi
-  if ((REDEPLOY_COUNT > 1)); then
-    koyeb service redeploy wrong-alexia/setup
-    echo "Đã redeploy lần thứ $REDEPLOY_COUNT"
-  else
-    echo "Chỉ thực hiện pause và resume lần đầu."
-  fi
-  ((REDEPLOY_COUNT++))
+# Đếm số lần pause và resume
+COUNT=1
+
+# Tạm dừng và resume dịch vụ
+pause_resume_service() {
+  echo "Lần thứ $COUNT: Tạm dừng và resume dịch vụ wrong-alexia/setup..."
+  koyeb service pause wrong-alexia/setup && sleep 17
+  koyeb service resume wrong-alexia/setup && sleep 3
+  ((COUNT++))
 }
 
-# Lần đầu tiên thực hiện hành động (chỉ pause và resume)
-redeploy_service
+# Lần đầu tiên thực hiện hành động (pause và resume)
+pause_resume_service
 
 # Đếm thời gian và thực hiện lại sau 9 phút 30 giây
 TIME_LEFT=570
 while ((TIME_LEFT--)); do
   sleep 1
-  echo "Thời gian còn lại tiếp tục redeploy: $TIME_LEFT giây"
+  echo "Thời gian còn lại tiếp tục pause và resume: $TIME_LEFT giây"
   if ((TIME_LEFT == 0)); then
-    # Thực hiện lại redeploy sau khi thời gian đếm ngược hoàn tất
-    redeploy_service
+    # Thực hiện lại pause và resume sau khi thời gian đếm ngược hoàn tất
+    pause_resume_service
     TIME_LEFT=570
   fi
-done
+done 
